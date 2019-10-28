3047bd11909981e13a8391735666daacbf474268
Ben Chen <bche536@aucklanduni.ac.nz>
2019-10-28 18:46:09 +1300

    deleted redundant files, commented all the code

--- src/stylesheets/creationObj.java
+++
@@ -1,9 +0,0 @@
-package stylesheets;
-
-public class creationObj {
-    private String _name, _searchTerm;
-    public creationObj(String name, String searchTerm) {
-        _name = name;
-        _searchTerm = searchTerm;
-    }
-}
